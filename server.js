fa890e44d8b413812c7a19091270436509b566ab
swvmpdad <will.g.camp@gmail.com>
2022-10-02 12:31:19 -0500

    coded server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = prcoess.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODBURI || 'mongodb://127.0.0.1:27017/creed-thoughts', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
