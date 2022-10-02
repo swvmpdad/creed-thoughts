e94530f114f84c77a73f07b3869b1407a867695b
swvmpdad <will.g.camp@gmail.com>
2022-10-02 15:02:01 -0500

    created route files

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const {
+    addThought,
+    addReaction,
+    removeThought,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+router.route('/:userId').post(addThought);
+
+router.route('/:userId/:thoughtId')
+    .put(addReaction)
+    .delete(removeThought);
+
+router.route('/:userId/:thoughtId/:replyId').delete(removeReaction);
+
+module.exports = router;
